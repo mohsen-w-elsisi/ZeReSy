646c1e0b97806570fe0c71040be07b1f85c2fabc
Mohsen Waheed Elsisi <mohsenelsisi36@gmail.com>
2026-04-23 23:07:16 +0200

    height of blocks on the exported calendar now reflects length of event

--- tests/schedule_exporter.cpp
+++ tests/schedule_exporter.cpp
@@ -7,8 +7,8 @@ const string FILE_NAME = "test_schedule_export.png";
 
 const Schedule MOCK_SCHEDULE({
 	Course("CS100", "Intro to AI", 3, "Dr. Smith", {{SUN, 9}}, 90, false).selectTime({SUN, 9}),
-	Course("MATH101", "Calculus I", 2, "Dr. Johnson", {{MON, 11}}, 90, false).selectTime({MON, 11}),
-	Course("PHYS120", "Physics I", 4, "Dr. Lee", {{WED, 14}}, 90, false).selectTime({WED, 14}),
+	Course("MATH101", "Calculus I", 2, "Dr. Johnson", {{MON, 11}}, 60, false).selectTime({MON, 11}),
+	Course("PHYS120", "Physics I", 4, "Dr. Lee", {{WED, 14}}, 120, false).selectTime({WED, 14}),
 });
 
 int main() {
